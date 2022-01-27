57d3bf4e08e2f5dcbf1182f0569f00387a523004
Sanjaya Paudel <paudelsanjaya702@gmail.com>
2022-01-27 11:26:49 +0545

    responsive added

--- assets/js/go_roam_main.js
+++ assets/js/go_roam_main.js
@@ -1,11 +1,59 @@
 $(".filter-btn").on("click", function () {
-	$("#filterList").slideToggle();
+  $("#filterList").slideToggle();
 });
 
 $("#goRoamBody").on("click", function (e) {
-	const targetEl = e.target;
-	const className = $(targetEl).attr("class");
-	if (className !== "filter-btn") {
-		$("#filterList").slideUp();
-	}
+  const targetEl = e.target;
+  const className = $(targetEl).attr("class");
+  if (className !== "filter-btn") {
+    $("#filterList").slideUp();
+  }
+});
+
+$(".nav-tab").on("click", function () {
+  const dataValue = $(this).attr("data-value");
+  if (dataValue === "open") {
+    $(".mobile-nav").animate({
+      left: "0px",
+      opacity: "1",
+    });
+    $(this).attr("data-value", "close");
+    $("#goRoamBody").addClass("fixed-body");
+  }
+  if (dataValue === "close") {
+    $(".mobile-nav").animate({
+      left: "-100%",
+      opacity: "0",
+    });
+    $(this).attr("data-value", "open");
+    $("#goRoamBody").removeClass("fixed-body");
+  }
+});
+
+$("#goRoamBody").on("click", function (e) {
+  const targetEl = e.target;
+  const className = $(targetEl).attr("class")?.split(" ")[0];
+  if (
+    className !== "nav-tab" &&
+    className !== "mobile-nav" &&
+    className !== "mb-link"
+  ) {
+    $(".mobile-nav").animate({
+      left: "-100%",
+      opacity: "0",
+    });
+    $(".nav-tab").attr("data-value", "open");
+    $("#goRoamBody").removeClass("fixed-body");
+  }
+});
+
+$(window).scroll(function () {
+  var bookRect = document.getElementById("bookBanner").getBoundingClientRect();
+  if ($(this).scrollTop() > 20) {
+    $(".book-wrapper").addClass("fixed-banner");
+  } else {
+    $(".book-wrapper").removeClass("fixed-banner");
+  }
+  console.log(bookRect);
+  console.log(document.getElementById("bookBanner").offsetTop);
 });
